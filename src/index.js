1b34df824699129ee7bad3f402f37820ff2dd8fa
techhtml <apes0123@gmail.com>
2019-12-21 08:32:17 +0900

    init(-): React Development Environment

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div className="app">Hello World</div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'));
